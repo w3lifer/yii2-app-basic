d9f81946a2cfdc31a56217aed721d98234191f58
Roman Grinyov <w3lifer@gmail.com>
2017-07-31 00:42:44 +0300

    added jquery cdn

--- config/components/assetManager.php
+++ config/components/assetManager.php
@@ -2,4 +2,12 @@
 
 return [
     'appendTimestamp' => true,
+    'bundles' => [
+        'yii\web\JqueryAsset' => [
+            'js' => [
+                'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/' .
+                (YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'),
+            ],
+        ],
+    ],
 ];
