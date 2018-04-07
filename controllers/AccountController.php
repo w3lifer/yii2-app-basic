fcc4c485acbd7abc4adcdad665fce23449c8deac
Roman Grinyov <w3lifer@gmail.com>
2018-04-07 13:02:41 +0300

    ::className() → ::class

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -25,7 +25,7 @@ class AccountController extends BaseController
     {
         return [
             'access' => [
-                'class' => AccessControl::className(),
+                'class' => AccessControl::class,
                 'rules' => [
                     [
                         'actions' => ['index', 'logout'],
@@ -45,7 +45,7 @@ class AccountController extends BaseController
                 ],
             ],
             'verbs' => [
-                'class' => VerbFilter::className(),
+                'class' => VerbFilter::class,
                 'actions' => [
                     'logout' => ['post'],
                 ],
